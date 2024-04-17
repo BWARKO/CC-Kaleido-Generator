c56943bae77acfc2f1e5c4d1d0a4aca289798cdd
Blake Warkenton <bwarkent@ucsc.edu>
2024-04-17 15:13:31 -0700

    UPDATED
    ADDED:
    - Watermark feature that shows seed and where you can access
    
    CHANGED:
    - Had to rework general logic so that it didn't keep regenerating the kaleidoscope everytime it saved

--- CC_Kaleido_Generator.js
+++ CC_Kaleido_Generator.js
@@ -1,20 +1,28 @@
 // Global variables
 let seed;
+let saveButton; 
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 800).center('horizontal');
   angleMode(DEGREES);
   noLoop();
 
-  seed = prompt("Please enter a seed for your kaleidoscope:");
-  randomSeed(hashCode(seed)); // Seed the pseudo-random generator again had chatgpt help me understand how to do this
+  seed = prompt("Please enter a seed for your kaleidoscope:"); // Seed the pseudo-random generator again had chatgpt help me understand how to do this
+  // Create saving button
+  saveButton = createButton('Save Image');
+  saveButton.position(400, 10);
+  saveButton.mousePressed(saveKaleidoscopeImage);
+
+  drawKaleidoscope(); // Draw seed kaleido
 }
 
-function draw() {
-  background(0);
-  translate(width / 2, height / 2); 
+function drawKaleidoscope() {
+  randomSeed(hashCode(seed)); // seed/reseed
+  resetMatrix(); // Reset trans
+  background(255);
+  translate(width / 2, height / 2);
   
-  const numSlices = 16; 
+  const numSlices = 16;
   const sliceAngle = 360 / numSlices;
   
   for (let i = 0; i < numSlices; i++) {
@@ -23,7 +31,7 @@ function draw() {
     drawSlice();
     pop();
     push();
-    scale(-1, 1); 
+    scale(-1, 1);
     rotate(sliceAngle * i);
     drawSlice();
     pop();
@@ -66,13 +74,29 @@ function drawSlice() {
   }
 }
 
+function saveKaleidoscopeImage() {
+  drawKaleidoscope(); 
+  drawWatermark(); 
+  saveCanvas('kaleidoscope', 'png');
+  drawKaleidoscope(); 
+}
+
+function drawWatermark() {
+  push();
+  textAlign(LEFT, BOTTOM);
+  textSize(16);
+  fill(255, 255, 255, 150); 
+  text('Create with SEED: "' + seed + '" at https://bwarko.github.io/CC-Kaleido-Generator/ ', -390, 390);
+  pop();
+}
+
 // A simple hashing function for the seed, had help developing and understanding with chatgpt
 function hashCode(str) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
     let char = str.charCodeAt(i);
     hash = ((hash << 5) - hash) + char;
-    hash |= 0; 
+    hash |= 0;
   }
   return hash;
 }
